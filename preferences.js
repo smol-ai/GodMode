c6a124ae350541b6ec2b34a743514e5a18870334
moismat <mohamedismat91@gmail.com>
2023-05-18 16:48:50 +0200

    feat: persist preferences

---
+++ preferences.js
@@ -0,0 +1,34 @@
+const Store = require('electron-store');
+const store = new Store();
+
+document.getElementById('webviewOAIEnabled').checked = store.get('webviewOAIEnabled', true);
+document.getElementById('webviewBARDEnabled').checked = store.get('webviewBARDEnabled', true);
+document.getElementById('webviewCLAUDEEnabled').checked = store.get('webviewCLAUDEEnabled', true);
+
+document.getElementById('webviewOAIEnabled').addEventListener('change', (event) => {
+    store.set('webviewOAIEnabled', event.target.checked);
+});
+
+document.getElementById('webviewBARDEnabled').addEventListener('change', (event) => {
+    store.set('webviewBARDEnabled', event.target.checked);
+});
+
+document.getElementById('webviewCLAUDEEnabled').addEventListener('change', (event) => {
+    store.set('webviewCLAUDEEnabled', event.target.checked);
+});
+
+document.getElementById('save').addEventListener('click', () => {
+
+    console.log('save clicked');
+
+    // Save the user's preferences
+    const webviewOAIEnabled = document.getElementById('webviewOAIEnabled').checked;
+    const webviewBARDEnabled = document.getElementById('webviewBARDEnabled').checked;
+    const webviewCLAUDEEnabled = document.getElementById('webviewCLAUDEEnabled').checked;
+    store.set('webviewOAIEnabled', webviewOAIEnabled);
+    store.set('webviewBARDEnabled', webviewBARDEnabled);
+    store.set('webviewCLAUDEEnabled', webviewCLAUDEEnabled);
+
+    // Close the preferences window
+    window.close();
+});
