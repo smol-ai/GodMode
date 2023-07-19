c82aa24204580f1e5ecec73c25f069fe5ecad5ba
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-19 15:37:47 -0700

    refactor(huggingchat) change default isEnabled state to false

--- providers/huggingchat.js
+++ providers/huggingchat.js
@@ -78,7 +78,7 @@ class HuggingChat extends Provider {
 	}
 
 	static isEnabled() {
-		return store.get(`${this.webviewId}Enabled`, true);
+		return store.get(`${this.webviewId}Enabled`, false);
 	}
 }
 
