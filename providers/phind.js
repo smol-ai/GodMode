552ce21ad9fa2dfa32a06e7104bd58160e374a41
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-25 09:57:54 -0700

    support for phind via native setter function

--- providers/phind.js
+++ providers/phind.js
@@ -11,23 +11,22 @@ class Phind extends Provider {
 
 	static handleInput(input) {
 		this.getWebview().executeJavaScript(`
-        function simulateUserInput(element, text) {
-          const inputEvent = new Event('input', { bubbles: true });
-          element.focus();
-          element.value = text;
-          element.dispatchEvent(inputEvent);
-        }
         var inputElement = document.querySelector('textarea[placeholder*="Describe your technical task"]');
+
+        function simulateUserInput(element, text) {
+          var nativeTextAreaValueSetter = Object.getOwnPropertyDescriptor(window.HTMLTextAreaElement.prototype, "value").set;
+          var event = new Event('input', { bubbles: true});
+
+          nativeTextAreaValueSetter.call(inputElement, "${input}");
+          inputElement.dispatchEvent(event);
+        }
+
         simulateUserInput(inputElement, "${input}");
       `);
 	}
 
 	static handleSubmit() {
 		this.getWebview().executeJavaScript(`
-      // focus the input element
-      var inputElement = document.querySelector('textarea[placeholder*="Describe your technical task"]');
-      inputElement.focus();
-
       // simulate keypress to trigger the submit button
       var keyEvent = new KeyboardEvent('keydown', {key: ' ', bubbles: true});
       inputElement.dispatchEvent(keyEvent);
@@ -47,50 +46,28 @@ class Phind extends Provider {
 
 	static handleCss() {
 		this.getWebview().addEventListener('dom-ready', () => {
-			// hide message below text input, sidebar, suggestions on new chat
         setTimeout(() => {
-          this.getWebview().executeJavaScript(`
-          // Add Dark Mode
-          document.documentElement.classList.add('dark');
+        this.getWebview().executeJavaScript(`
+        // Add Dark Mode
+        const page = document.documentElement;
+        page.setAttribute('data-theme', 'dark');
+        page.setAttribute('style', 'color-scheme: dark;');
 
-          // Hide Examples Box
-          var elements = Array.from(document.querySelectorAll('div[class]'));
-          var targetElement;
+        // Hide Phind Logo
+        const images = document.querySelectorAll('img[src*="phind"]');
+        images[images.length - 1].setAttribute('style', 'display: none;');
 
-          for (var i = 0; i < elements.length; i++) {
-              var classes = elements[i].className.split(' ');
-              if (classes.includes('lg:col-span-3') && classes.includes('lg:mt-12') && elements[i].textContent.includes('Examples')) {
-                  targetElement = elements[i];
-                  break;
-              }
-          }
+        // Hide Tagline
+        const tagline = document.querySelector('h1');
+        tagline.setAttribute('style', 'display: none;');
 
-          if (targetElement) {
-            targetElement.style.display = 'none';
-          }
+        // Hide Explore Options
+        const exploreOptions = document.querySelector('div.container:has(h4)');
+        exploreOptions.setAttribute('style', 'display: none;');
 
-          // Hide Phind Logo
-          var elements = Array.from(document.querySelectorAll('div object'));
-
-          elements.forEach(element => {
-            if (element.parentElement.textContent.includes('Phind')) {
-              element.parentElement.parentElement.style.display = 'none';
-            }
-          });
-
-          // Same loop for the other text.
-          var pElements = Array.from(document.querySelectorAll('p'));
-
-          pElements.forEach(element => {
-            if (element.textContent.includes('Examples')) {
-              element.parentElement.style.display = 'none';
-            }
-          });
-
-
-          `);
-        }, 100);
-		});
+        `);
+      }, 100);
+      });
 	}
 
 	static isEnabled() {
