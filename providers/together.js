d3b5562a91c4f2695ec50e834ebf20483a55ec4e
swyxio <swyxio@users.noreply.github.com>
2023-07-31 05:46:43 +0000

    Apply prettier changes

--- providers/together.js
+++ providers/together.js
@@ -5,8 +5,7 @@ const Provider = require('./provider');
 
 class Together extends Provider {
 	static webviewId = 'webviewTOGETHER';
-  static fullName = 'Together (WIP - RedPajama, StarCoder, Falcon, etc)';
-
+	static fullName = 'Together (WIP - RedPajama, StarCoder, Falcon, etc)';
 
 	static url = 'https://api.together.xyz/playground/chat';
 
@@ -43,20 +42,20 @@ class Together extends Provider {
 		// 	// hide message below text input, sidebar, suggestions on new chat
 		// 	setTimeout(() => {
 		// 		this.getWebview().insertCSS(`
-    //     header, .container {
-    //       background-color: white;
-    //       /* single line dark mode ftw */
-    //       filter: invert(100%) hue-rotate(180deg);
-    //     }
-    //     /* hide the claude avatar in response */
-    //     .p-1.w-9.h-9.shrink-0 {
-    //       display: none;
-    //     }
-    //     /* reduce claude prompt margins */
-    //     .mx-4.md\:mx-12.mb-2.md\:mb-4.mt-2.w-auto {
-    //       margin: 0 !important;
-    //     }
-    //     `);
+		//     header, .container {
+		//       background-color: white;
+		//       /* single line dark mode ftw */
+		//       filter: invert(100%) hue-rotate(180deg);
+		//     }
+		//     /* hide the claude avatar in response */
+		//     .p-1.w-9.h-9.shrink-0 {
+		//       display: none;
+		//     }
+		//     /* reduce claude prompt margins */
+		//     .mx-4.md\:mx-12.mb-2.md\:mb-4.mt-2.w-auto {
+		//       margin: 0 !important;
+		//     }
+		//     `);
 		// 	}, 1000);
 		// });
 	}
