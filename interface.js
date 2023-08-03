8a628306c16a70321a5bdcef86782b1f93306f87
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-08-02 17:37:08 -0700

    feat(interface) add dark mode toggle, refactor(interface) move getEnabledProviders

--- interface.js
+++ interface.js
@@ -3,6 +3,7 @@ const { ipcRenderer } = require('electron');
 const log = require('electron-log');
 const Store = require('electron-store');
 const store = new Store();
+const { getEnabledProviders, getAllProviders } = require('./src/utils');
 
 // Initialize the providers
 const providers = {
@@ -20,7 +21,6 @@ const providers = {
 
 const {
 	drawPanes,
-	getEnabledProviders,
 	updateSplitSizes,
 } = require('./src/panes');
 
@@ -51,17 +51,17 @@ window.addEventListener('DOMContentLoaded', () => {
 /* Dark Mode Listeners                                                        */
 /* ========================================================================== */
 
-// Get the initial value of isDarkMode from the store
-ipcRenderer.invoke('getStoreValue', 'isDarkMode').then((isDarkMode) => {
-    enabledProviders.forEach((provider) => provider.toggleDarkMode(isDarkMode));
-});
+// // Get the initial value of isDarkMode from the store
+// ipcRenderer.invoke('getStoreValue', 'isDarkMode').then((isDarkMode) => {
+//     enabledProviders.forEach((provider) => provider.toggleDarkMode(isDarkMode));
+// });
 
-// Listen for changes to the isDarkMode value in the store
-ipcRenderer.on('setStoreValue', (event, key, value) => {
-    if (key === 'isDarkMode') {
-        enabledProviders.forEach((provider) => provider.toggleDarkMode(value));
-    }
-});
+// // Listen for changes to the isDarkMode value in the store
+// ipcRenderer.on('setStoreValue', (event, key, value) => {
+//     if (key === 'isDarkMode') {
+//         enabledProviders.forEach((provider) => provider.toggleDarkMode(value));
+//     }
+// });
 
 /* ========================================================================== */
 /* Prompt Input Listeners                                                     */
@@ -143,8 +143,28 @@ document.addEventListener('keydown', (event) => {
 				.getWebview()
 				.setZoomLevel(provider.getWebview().getZoomLevel() - 1);
 		});
+	} else if ((event.shiftKey && event.metaKey && (event.key === 'L' || event.key === 'l'))) {
+		// Toggle dark mode with Cmd/Ctrl + Shift + L
+		let isDarkMode = false;
+		ipcRenderer.invoke('getStoreValue', 'isDarkMode').then((currentDarkMode) => {
+			getAllProviders(providers).forEach((provider) => {
+				ipcRenderer.invoke('setStoreValue', 'isDarkMode', !currentDarkMode);
+				isDarkMode = !currentDarkMode;
+			});
+		});
+		
+		console.log('🔵 isDarkMode', isDarkMode);
+		try {
+			enabledProviders.forEach(provider => provider.handleDarkMode(isDarkMode));
+		} catch (err) {
+			console.error('🔴🔴 error: ', err);
+		}
+
 	}
 });
+
+
+
 
 document.addEventListener('keydown', function (event) {
 	if (event.shiftKey && event.metaKey && event.keyCode === 70) {
