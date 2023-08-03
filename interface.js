b52106764bbba9f7182ab1d8d3dc7128a8acacd4
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-08-02 17:55:46 -0700

    refactor(interface) simplify dark mode toggle and remove unecessary console logs

--- interface.js
+++ interface.js
@@ -20,10 +20,7 @@ const providers = {
 	Smol: require('./providers/smol'),
 };
 
-const {
-	drawPanes,
-	updateSplitSizes,
-} = require('./src/panes');
+const { drawPanes, updateSplitSizes } = require('./src/panes');
 
 /* ========================================================================== */
 /* Create Panes                                                               */
@@ -146,28 +143,21 @@ document.addEventListener('keydown', (event) => {
 				.getWebview()
 				.setZoomLevel(provider.getWebview().getZoomLevel() - 1);
 		});
-	} else if ((event.shiftKey && event.metaKey && (event.key === 'L' || event.key === 'l'))) {
+	} else if (
+		event.shiftKey &&
+		event.metaKey &&
+		(event.key === 'L' || event.key === 'l')
+	) {
 		// Toggle dark mode with Cmd/Ctrl + Shift + L
-		let isDarkMode = false;
-		ipcRenderer.invoke('getStoreValue', 'isDarkMode').then((currentDarkMode) => {
-			getAllProviders(providers).forEach((provider) => {
-				ipcRenderer.invoke('setStoreValue', 'isDarkMode', !currentDarkMode);
-				isDarkMode = !currentDarkMode;
-			});
-		});
-		
-		console.log('🔵 isDarkMode', isDarkMode);
-		try {
-			enabledProviders.forEach(provider => provider.handleDarkMode(isDarkMode));
-		} catch (err) {
-			console.error('🔴🔴 error: ', err);
-		}
+		let isDarkMode = store.get('isDarkMode', false);
+		isDarkMode = !isDarkMode;
+		store.set('isDarkMode', isDarkMode);
 
+		enabledProviders.forEach((provider) => {
+			provider.handleDarkMode(isDarkMode);
+		});
 	}
 });
-
-
-
 
 document.addEventListener('keydown', function (event) {
 	if (event.shiftKey && event.metaKey && event.keyCode === 70) {
