4b2f5dc0d399f8abd77c9a45b367580dffc14d62
swyxio <shawnthe1@gmail.com>
2023-08-17 18:58:08 -0700

    publishable notarizable build

--- scripts/scripts/notarize.js
+++ scripts/scripts/notarize.js
@@ -21,10 +21,15 @@ exports.default = async function notarizeMacos(context) {
 
 	const appName = context.packager.appInfo.productFilename;
 
+	const appPath =  `${appOutDir}/${appName}.app`;
 	await notarize({
-		appBundleId: build.appId,
-		appPath: `${appOutDir}/${appName}.app`,
-		appleId: process.env.APPLE_ID,
-		appleIdPassword: process.env.APPLE_ID_PASS,
+		appPath,
+		// appleId: process.env.APPLE_ID,
+		// appleIdPassword: process.env.APPLE_ID_PASS,
+		tool: 'notarytool',
+		// https://www.electronforge.io/guides/code-signing/code-signing-macos
+		appleApiKey: `build/AuthKey_QRJN4Y5JG6.p8`,
+		appleApiKeyId: 'QRJN4Y5JG6',
+		appleApiIssuer: "58a60bb5-5495-4a08-b180-2c106e11bc66"
 	});
 };
