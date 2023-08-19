a3b963890d8480593bc1bc8d421ba9e67adc19ee
Alex Atallah <alexander.atallah@gmail.com>
2023-08-19 17:39:50 -0400

    add openrouter

---
+++ src/providers/openrouter.js
@@ -0,0 +1,58 @@
+const Provider = require('./provider');
+
+class OpenRouter extends Provider {
+	static webviewId = 'webviewOpenRouter';
+	static fullName = 'OpenRouter Playground';
+	static shortName = 'OpenRouter';
+
+	static url = 'https://openrouter.ai/playground';
+
+	static handleInput(input) {
+		const fullName = this.fullName;
+		this.getWebview().executeJavaScript(`{
+        var inputElement = document.querySelector('textarea[placeholder*="Chat or prompt"]'); // can be "Ask anything" or "Ask follow-up"
+        if (!inputElement) {
+          console.error('inputElement for ${fullName} doesnt exist, have you logged in or are you on the right page?')
+        } else {
+					var nativeTextAreaValueSetter = Object.getOwnPropertyDescriptor(window.HTMLTextAreaElement.prototype, "value").set;
+					nativeTextAreaValueSetter.call(inputElement, \`${input}\`);
+	
+					var event = new Event('input', { bubbles: true});
+					inputElement.dispatchEvent(event);
+        }
+    }`);
+	}
+
+	static handleSubmit() {
+		this.getWebview().executeJavaScript(`{
+    var buttons = Array.from(document.querySelectorAll('button'));
+    var buttonsWithSVGOnly = buttons.filter(button => {
+      var svg = button.querySelector('svg');
+      return !!svg;
+    });
+
+    if (buttonsWithSrOnly.length == 1){
+      var button = buttonsWithSrOnly[0];
+      button.click();
+    }
+  }`);
+	}
+	static handleCss() {
+		this.getWebview().addEventListener('dom-ready', () => {
+			// Hide the "Try asking" segment
+			setTimeout(() => {
+				this.getWebview().insertCSS(`
+        .mt-lg {
+          display: none;
+        }
+		    `);
+			}, 100);
+		});
+	}
+
+	static isEnabled() {
+		return window.electron.electronStore.get(`${this.webviewId}Enabled`, false);
+	}
+}
+
+module.exports = OpenRouter;
