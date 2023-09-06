26574abca3c58319a61016bf0680ae8f02206b6e
swyxio <swyxio@users.noreply.github.com>
2023-09-06 08:46:20 +0000

    Apply prettier changes

--- src/providers/bard.js
+++ src/providers/bard.js
@@ -90,7 +90,7 @@ class Bard extends Provider {
 	}
 
 	static getUserAgent() {
-    // bard does not accept Electron mentions.
+		// bard does not accept Electron mentions.
 		return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36';
 	}
 
